fb6bd94f7c694d520affbc443c3968bffc42f25e
Samuel581 <00023020@uca.edu.sv>
2025-11-17 19:57:35 -0600

    :sparkles: Add movie details endpoint and types

--- src/lib/api/tmdb.api.ts
+++ src/lib/api/tmdb.api.ts
@@ -1,11 +1,32 @@
-import type { Movie, PaginatedResponse } from "../../types/movie.types";
+import type { Movie, MovieDetails, PaginatedResponse } from "../../types/movie.types";
 import { apiClient } from "./client";
 
 
 export const tmdbApi = {
+
+  // Movies list 
+  // Get popular movie
   getPopularMovies: async (page: number = 1): Promise<PaginatedResponse<Movie>> => {
     return apiClient.get("movie/popular", {
       params: { page }
     })
+  },
+
+  // Get movies currently being played in thearers
+  getOnThearersMovies: async (page: number = 1): Promise<PaginatedResponse<Movie>> => {
+    return apiClient.get("movie/now_playing", {
+      params: { page }
+    })
+  },
+
+  // Movies details 
+  // Details 
+  getMovieDetails: async (movie_id: number): Promise<MovieDetails> => {
+    return apiClient.get(`movie/${movie_id}`, {
+    })
   }
+
+
 }
+
+
