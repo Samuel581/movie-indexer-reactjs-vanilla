f870aec1571f29c057f241a92295b187ccf646fa
Samuel581 <00023020@uca.edu.sv>
2025-11-30 20:16:09 -0600

    :sparkles: Add: fecth popular movies hook

---
+++ src/features/PopularMovies/hooks/useFetchMovies.ts
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import { tmdbApi } from "../../../lib/api/tmdb.api";
+import type { Movie } from "../../../types/movie.types";
+
+export function useFecthMovies() {
+  const [movies, setMovies] = useState<Movie[]>([])
+  const [loading, setLoading] = useState<boolean>(true)
+  const [error, setError] = useState<unknown | null>(null)
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        setLoading(true);
+        const movies = await tmdbApi.getPopularMovies()
+        setMovies(movies.results)
+      }
+      catch (err) {
+        setError(err)
+      }
+      finally {
+        setLoading(false)
+      }
+    };
+    fetchMovies()
+  }, [])
+
+  return { movies, loading, error }
+}
