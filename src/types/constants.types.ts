6c5fe1bd805e946f5e9bc8512bce6603f6d8a3b2
Samuel581 <00023020@uca.edu.sv>
2025-12-18 22:16:21 -0600

    :label: Update image base URLs
    Refactores image base URLs to clearer names and also include
    compatibility for backdrop including a BACKDROP base URL

--- src/types/constants.types.ts
+++ src/types/constants.types.ts
@@ -1,1 +1,2 @@
-export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500"
+export const POSTER_BASE_URL = "https://image.tmdb.org/t/p/w500";
+export const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/w1280";
