54215f63ac5d2063cf919b5c1169c05b6e70d6d5
Samuel581 <00023020@uca.edu.sv>
2025-12-27 20:33:06 -0600

    :sparkles: Update movie details hooks to handle null movie

--- src/features/MovieDetails/hooks/useFetchMovieDetails.ts
+++ src/features/MovieDetails/hooks/useFetchMovieDetails.ts
@@ -2,12 +2,14 @@ import { useEffect, useState } from "react";
 import type { MovieDetails } from "../../../types/movie.types";
 import { tmdbApi } from "../../../lib/api/tmdb.api";
 
-export function useFetchMovieDetails(movieId: number) {
+export function useFetchMovieDetails(movieId: number | null) {
   const [movie, setMovie] = useState<MovieDetails>();
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<unknown | null>(null);
 
   useEffect(() => {
+    if (!movieId) return;
+
     const fetchMovieDetails = async () => {
       try {
         setIsLoading(true);
