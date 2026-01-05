28d10ba68008dbf2b323cb7ebd7006745cfa1111
Samuel581 <00023020@uca.edu.sv>
2026-01-04 21:14:18 -0600

    :necktie: Configure router and initial routes
    Configure react router wrapper and include movie details route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router";
 import { MasterScreen } from "./features/MasterScreen";
+import MovieDetailsPage from "./features/MovieDetails/MovieDetailsPage";
 
 function App() {
   return (
-    <>
-      <MasterScreen />
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MasterScreen />} />
+        <Route path="/movies/:id" element={<MovieDetailsPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
